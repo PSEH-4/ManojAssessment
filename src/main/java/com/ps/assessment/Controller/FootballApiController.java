ee71ee4d246aae28b1aad1573a53be39a678583f
India Hiring <indiahiring@global.publicisgroupe.net>
2019-09-28 14:07:24 +0530

    compilation

--- src/main/java/com/ps/assessment/Controller/FootballApiController.java
+++ src/main/java/com/ps/assessment/Controller/FootballApiController.java
@@ -4,7 +4,6 @@ import com.ps.assessment.Exception.MatchNotFoundException;
 import com.ps.assessment.model.TeamStanding;
 import com.ps.assessment.service.FootballApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,9 +16,9 @@ public class FootballApiController {
     private FootballApiService footballApiService;
 
     @RequestMapping(value = "/overallStandings/{countryName}/{leagueName}/{teamName}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public ResponseEntity getOverallStandings(@PathVariable String countryName,
-                                              @PathVariable String leagueName,
-                                              @PathVariable String teamName) throws MatchNotFoundException {
+    public TeamStanding getOverallStandings(@PathVariable String countryName,
+                                            @PathVariable String leagueName,
+                                            @PathVariable String teamName) throws MatchNotFoundException {
         return footballApiService.getOverallPosition(countryName, leagueName, teamName);
     }
 
